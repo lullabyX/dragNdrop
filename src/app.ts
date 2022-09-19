683de449399a57e66706434998e69b7d47bfa4f9
Hassan Rabbi <hassan_rabbi@live.com>
2022-09-19 19:26:23 +0600

    create autobinder decorator

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor): PropertyDescriptor {
+  const method = descriptor.value;
+  
+  return {
+    configurable: true,
+    enumerable: false,
+    get() {
+      return method.bind(this);
+    }
+  }
+}
 class Project {
   hostElement: HTMLDivElement;
   projectInputTemplateElement: HTMLTemplateElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputeElement: HTMLInputElement;
+  peopleInputeElement: HTMLInputElement;
 
   constructor() {
     this.hostElement = document.getElementById("app") as HTMLDivElement;
@@ -9,14 +23,39 @@ class Project {
       "project-input"
     ) as HTMLTemplateElement;
 
-    const insertedNode = document.importNode(this.projectInputTemplateElement.content, true);
-    this.element = insertedNode.firstElementChild as HTMLFormElement
+    const insertedNode = document.importNode(
+      this.projectInputTemplateElement.content,
+      true
+    );
+    this.element = insertedNode.firstElementChild as HTMLFormElement;
+    this.element.setAttribute("id", "user-input");
 
-    this.attach()
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputeElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputeElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  @Autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
   private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
 
